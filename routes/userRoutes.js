d3d316cb395415504321bbd454414ef5ff3e696b
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-02-02 14:00:28 -0500

    created post route for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,27 @@
+const mongoose = require("mongoose");
+const routes = require("express").Router();
+const Users = require("/users.js")
+
+routes.post("/", (req, res) => {
+    
+    const usr_name = req.body.usr_name;
+    const profile_photo = req.body.profile_photo;
+    const photos = req.body.photos;
+    const bio = req.body.bio;
+
+
+    const user = Users.create({
+        usr_name,
+        profile_photo,
+        photos,
+        bio
+    },
+    (err, data ) => {
+        if (err) return res.status(500).send(err.message)
+        res.status(200).json(data)
+        
+    })
+
+    
+});
 
