0a48841b3a31b5a378c12ca91e7c2f0202ce5025
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-02-15 14:51:42 -0500

    added admin component

--- client/src/components/SideNavBar/SideNav.jsx
+++ client/src/components/SideNavBar/SideNav.jsx
@@ -1,20 +1,41 @@
-
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import {
+  AppstoreOutlined,
+  MailOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
 import Login from "../../containers/Login/Login";
 import Feed from "../../containers/Feed/Feed";
 import styles from "./style.module.css";
-import { Row, Col } from 'antd';
+import { Row, Col } from "antd";
 
-
-
-const SideNav = ()=> {
+const SideNav = () => {
   // const handleClick = e => {
   //       console.log('click ', e);
   //     };
-    return (
-      <Router>
+  return (
+    <Router>
       <div className={styles.wrapper}>
+        <li>
+          <Link to="/login">About</Link>
+        </li>
+        <li>
+          <Link to="/feed">SEE WHO'S USING ARTHOUSE</Link>
+        </li>
+      </div>
+
+      {/* <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/feed">
+          <Col span={12} offset={6}>
+            {" "}
+            <Feed />
+          </Col>
+        </Route>
+      </Switch> */}
+      {/* <div className={styles.wrapper}>
 
         <li>
           <Link to="/login">About</Link>
@@ -31,9 +52,9 @@ const SideNav = ()=> {
         <Route path="/feed">
         <Col span={12} offset={6}>  <Feed /></Col>
         </Route>
-      </Switch>
+      </Switch> */}
     </Router>
-    )
-}
+  );
+};
 
 export default SideNav;
