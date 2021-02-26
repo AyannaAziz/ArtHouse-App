c4376ab94d27f3d5c451a91683a16777e3bd97f8
Ayanna Aziz <ayanna.aziz77@gmail.com>
2021-02-25 23:01:06 -0500

    updated about page

--- client/src/containers/About/About.jsx
+++ client/src/containers/About/About.jsx
@@ -6,14 +6,7 @@ const About = () => {
     <div className="row">
       <div className="col">
         <h1 className="center-align">
-          Welcome to ArtHouse. ArtHouse is a blah blah...Lorem ipsum dolor sit
-          amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est
-          laborum.
+          Welcome to ArtHouse. ArtHouse is a workspace curated by creatives for creatives. We are still building this experience. 
         </h1>
       </div>
     </div>
